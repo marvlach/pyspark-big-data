6dbb1d9acfe248c54bf4f5fb78eaccb7c8028042
marvlach <marios.vlachog@gmail.com>
2024-05-19 12:42:13 +0300

    Query 1: sql and report results

--- src/query1.py
+++ src/query1.py
@@ -29,21 +29,9 @@ def read_from_parquet(spark):
     return df
 
 
-def main(file_format, spark_api):
-    spark = SparkSession.builder.appName(
-        f"query1 {file_format} {spark_api}"
-    ).getOrCreate()
-
-    start_time = time.time()
-
-    # get RDD based on user-defined file_format
-    df_rdd = (
-        read_from_csv(spark) if file_format == "csv" else read_from_parquet(spark)
-    ).rdd
-
-    # query
+def query_mr(df):
     query = (
-        df_rdd.map(
+        df.rdd.map(
             lambda row: (
                 (row["Date Rptd"][6:10], row["Date Rptd"][:2]),
                 1,
@@ -64,6 +52,53 @@ def main(file_format, spark_api):
         )  # (year, month, crimes, index)
     )
     print(query.collect())
+
+
+def query_sql(df, spark):
+    df.registerTempTable("crimes")
+    query = """
+    select month, year, count, rank
+    from (
+        select 
+            month,
+            year,
+            count,
+            row_number() over (partition by year order by count desc) rank
+            from (
+                SELECT month, year, count(*) as count
+                from (
+                    SELECT 
+                        substring(`Date Rptd`, 7, 4) as year,
+                        substring(`Date Rptd`, 0,2) as month 
+                    FROM crimes
+                ) crimes_ym
+                GROUP BY year, month
+        ) ymc
+    )
+    where rank < 4
+    order by year ASC, rank ASC
+    """
+
+    res = spark.sql(query)
+    res.show(50)
+
+
+def main(file_format, spark_api):
+    spark = SparkSession.builder.appName(
+        f"query1 {file_format} {spark_api}"
+    ).getOrCreate()
+
+    start_time = time.time()
+
+    # get DF based on user-defined file_format
+    df = read_from_csv(spark) if file_format == "csv" else read_from_parquet(spark)
+
+    # query based on user-defined spark api
+    if spark_api == "mr":
+        query_mr(df)
+    else:
+        query_sql(df, spark)
+
     print("time elapsed", time.time() - start_time)
     spark.stop()
 
@@ -78,8 +113,8 @@ if __name__ == "__main__":
         print("Usage: First argument must be literals: csv or parquet", file=sys.stderr)
         sys.exit(-1)
 
-    if spark_api not in {"df", "sql"}:
-        print("Usage: Second argument must be literals: df or sql", file=sys.stderr)
+    if spark_api not in {"mr", "sql"}:
+        print("Usage: Second argument must be literals: mr or sql", file=sys.stderr)
         sys.exit(-1)
 
     main(file_format, spark_api)
