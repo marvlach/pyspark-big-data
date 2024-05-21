59e94548bdda455415c91ee03f8cc51a4ad61f0e
marvlach <marios.vlachog@gmail.com>
2024-05-22 00:09:50 +0300

    Query 3: results

--- src/query3.py
+++ src/query3.py
@@ -1,12 +1,7 @@
-from pyspark.sql import SparkSession
+import sys
 import time
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, regexp_replace
-
-
-def query_sql(df, spark): ...
-
-
-def query_df(df): ...
 
 
 def get_descent_df(spark):
@@ -52,12 +47,12 @@ def get_crimes_df(spark):
     ) # result 0
     ```
     """
+    paths = [
+        "hdfs://master:9000/home/user/datasets/crime_data_2010s.parquet",
+        "hdfs://master:9000/home/user/datasets/crime_data_2020s.parquet",
+    ]
     crimes_df = (
-        spark.read.csv(
-            "./data/Crime_Data_2010s.csv",
-            header=True,
-            inferSchema=True,
-        )
+        spark.read.parquet(*paths)
         .filter((col("Vict Descent") != "-") & (col("Vict Descent").isNotNull()))
         # this slice [7:4] isn't python; it's overloaded
         .filter(col("DATE OCC")[7:4] == "2015")
@@ -77,10 +72,8 @@ def get_income_df(spark):
     - Zip Code: rename to Zipcode
     """
     income_df = (
-        spark.read.csv(
-            "./data/LA_income_2015.csv",
-            header=True,
-            inferSchema=True,
+        spark.read.parquet(
+            "hdfs://master:9000/home/user/datasets/LA_income_2015.parquet"
         )
         .withColumn("Zip Code", col("Zip Code").cast("string"))
         .withColumn(
@@ -94,40 +87,34 @@ def get_income_df(spark):
 
 def get_revgecoding_df(spark):
     """
+    - The 4th column is automatically dropped by the inference step.
+    As a result, (LAT, LON) pairs are unique
+
     - some pairs (LAT, LON) have NULL ZIPcode, but we need ZIPcode to join to income
 
     - some pairs (LAT, LON) have a ZIP-4 code, which according to Wikipedia
     "it includes the five digits of the ZIP Code,
     followed by a hyphen and four digits that designated a more specific location.".
-    We drop these last 4 digits
-
-    - (LAT, LON) pairs are unique
-
-    ```
-    print('yo1', revgeo_df.distinct().count()) # result 37416
-
-    print('yo2', revgeo_df.select(col("LAT"), col("LON")).distinct().count()) # result 37416
-    ```
+    We drop these last 4 digits.
+    BEWARE: THIS AFFECTS THE QUERY RESULTS
+    
     """
     revgeo_df = (
-        spark.read.csv(
-            "./data/revgecoding.csv",
-            header=True,
-            inferSchema=True,
-        )
-        .filter(col("ZIPcode").isNotNull())
-        .withColumn("ZIPcode", col("ZIPcode")[0:5])
+        spark.read.parquet(
+            "hdfs://master:9000/home/user/datasets/revgecoding.parquet"
+        ).filter(col("ZIPcode").isNotNull())
+        #.withColumn("ZIPcode", col("ZIPcode")[0:5]) # this affects query result
+        .withColumn("ZIPcode", col("ZIPcode"))
     )
     return revgeo_df
 
 
-def main():
+def main(income):
     spark = SparkSession.builder.appName("read_and_print").getOrCreate()
     start_time = time.time()
 
     # get dataframes
     crimes_df = get_crimes_df(spark)
-    # print(f"crimes_df rows: {crimes_df.distinct().count()}")
 
     revgeo_df = get_revgecoding_df(spark)
 
@@ -137,59 +124,54 @@ def main():
 
     # inner join to avoid NULL ZIPcode
     crimes_join_revgeo_df = crimes_df.join(revgeo_df, ["LAT", "LON"])
-    # print(f"crimes_join_revgeo_df rows: {crimes_join_revgeo_df.distinct().count()}")
 
     # inner join to avoid NULL Estimated Median Income
     crimes_join_revgeo_join_income_df = crimes_join_revgeo_df.join(
         income_df,
         "ZIPcode",
     )
-    # print(
-    #    "crimes_join_revgeo_join_income_df rows: "
-    #    f"{crimes_join_revgeo_join_income_df.distinct().count()}"
-    # )
 
-    # print(crimes_join_revgeo_join_income_df.take(1))
+    # sorted (zip code, income)
+    # top/bot income_df zip codes may not exist in main dataset
+    # use joined dataset income instead
+    distinct_zip_inc = crimes_join_revgeo_join_income_df.select(
+        col("ZIPcode"), col("Estimated Median Income")
+    ).distinct()
 
-    #
-    # print(crimes_join_revgeo_join_income_df.select(col('ZIPcode'), col('Estimated Median Income')).distinct().count())
-    # print(crimes_join_revgeo_join_income_df.select(col('ZIPcode')).distinct().count())
-
-    # group by (ZIPcode, Estimated Median Income, Vict Descent) and count
-    zc_inc_vic_count = crimes_join_revgeo_join_income_df.groupBy(
-        col("ZIPcode"), col("Estimated Median Income"), col("Vict Descent")
-    ).count()
-
-    # join with nice description
-    zc_inc_vic_count = zc_inc_vic_count.join(victim_descend_df, "Vict Descent").select(
-        col("ZIPcode"),
-        col("Estimated Median Income"),
-        col("Victim Descent"),
-        col("count"),
-    )
-    # sort incomes found
-    sorted_incomes = (
-        zc_inc_vic_count.select(col("Estimated Median Income"), col("ZIPcode"))
-        .distinct()
-        .orderBy(col("Estimated Median Income").desc())
+    sorted_zips_by_income = (
+        distinct_zip_inc.sort(col("Estimated Median Income").desc())
+        if income == "top"
+        else distinct_zip_inc.sort(col("Estimated Median Income").asc())
     )
 
     # top 3
-    top_3_income_zip_codes = [row["ZIPcode"] for row in sorted_incomes.head(3)]
-    top_3 = zc_inc_vic_count.filter(
-        col("ZIPcode").isin(top_3_income_zip_codes)
-    ).orderBy(col("Estimated Median Income").desc(), col("count").desc())
-    top_3.show(100)
-    top_3.explain()
+    where_zips = [row["ZIPcode"] for row in sorted_zips_by_income.head(3)]
+    result = (
+        crimes_join_revgeo_join_income_df.filter(col("ZIPcode").isin(where_zips))
+        .join(victim_descend_df, "Vict Descent")
+        .groupBy(col("Victim Descent"))
+        .count()
+        .orderBy(col("count").desc())
+        .select(col("Victim Descent"), col("count"))
+    )
 
-    # bot 3
-    bot_3_income_zip_codes = [row["ZIPcode"] for row in sorted_incomes.tail(3)]
-    bot_3 = zc_inc_vic_count.filter(
-        col("ZIPcode").isin(bot_3_income_zip_codes)
-    ).orderBy(col("Estimated Median Income").desc(), col("count").desc())
-    bot_3.show(100)
-
+    result.show(50)
+    print(f"{income} 3 income zips: {where_zips}")
     print("time elapsed", time.time() - start_time)
 
+    spark.stop()
 
-main()
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: query2.py top/bot", file=sys.stderr)
+        sys.exit(-1)
+
+    _, income = sys.argv
+
+    if income not in {"bot", "top"}:
+        print("Usage: Argument must be literals: top or bot", file=sys.stderr)
+        sys.exit(-1)
+
+    main(income)
